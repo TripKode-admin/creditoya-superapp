1d8ee8a305b2f443827affbec7974b7200e57d5f
FoulTrip <davidvasquezmahecha@gmail.com>
2025-06-03 09:14:51 -0500

    refactor: simplify Dockerfile build commands and clean up config files

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  // assetPrefix: './',
-  distDir: 'mobile-dist'
+  }
 };
 
 export default nextConfig;
