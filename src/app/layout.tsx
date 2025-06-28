11e2f399b6ed85b5bca9b069a851d3effb6289a6
FoulTrip <davidvasquezmahecha@gmail.com>
2025-06-28 10:51:36 -0500

    feat: add Geist font and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 import NavBar from "@/components/gadgets/NavBar";
 import { DarkModeProvider } from "@/context/DarkModeContext";
 import { ClientAuthProvider } from "@/context/AuthContext";
 import { Toaster } from "sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Credito Ya",
@@ -29,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={GeistSans.className}
       >
         <ClientAuthProvider>
           <DarkModeProvider>
